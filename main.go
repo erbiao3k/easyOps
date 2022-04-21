58394f48bb8adc034c4afb481117e51f4d225151
erbiao3k <1041876270@qq.com>
2022-04-21 22:52:26 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "easyOps/utils/ping"
+
+func main() {
+	ping.GoPing()
+	// tracert
+	// telnet
+	// nslookup
+	// whois
+	// mysql-client
+	// redis-client
+	// mongodb-client
+	//arp
+	//nmap
+}
